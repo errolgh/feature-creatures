de884722bda8da7529e949d0de435d172b372358
errolgh <ewatson@ggoutfitters.com>
2020-12-26 22:48:48 -0500

    successfully configured bootstrap sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 import { Button, Container, Alert } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 const IndexPage = () => (
   <Layout>
